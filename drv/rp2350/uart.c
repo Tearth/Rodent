26c56fddd82aba0a0fb61d79b38b086eb9fe661f
Tearth <tearthdev@gmail.com>
2026-08-05 20:21:18 +0200

    Add UART functions

---
+++ drv/rp2350/uart.c
@@ -0,0 +1,126 @@
+#include "uart.h"
+
+static const uart_into_t uart_info[] =
+{
+    // UART0
+    {
+        .reg_dr = UART0_REG_DR,
+        .reg_fr = UART0_REG_FR,
+        .reg_ibrd = UART0_REG_IBRD,
+        .reg_fbrd = UART0_REG_FBRD,
+        .reg_lcr = UART0_REG_LCR,
+        .reg_cr = UART0_REG_CR
+    },
+    // UART1
+    {
+        .reg_dr = UART1_REG_DR,
+        .reg_fr = UART1_REG_FR,
+        .reg_ibrd = UART1_REG_IBRD,
+        .reg_fbrd = UART1_REG_FBRD,
+        .reg_lcr = UART1_REG_LCR,
+        .reg_cr = UART1_REG_CR
+    },
+};
+
+bool uart_enable(uart_t uart, uint32_t baudrate, uint8_t data_bits, uint8_t stop_bits)
+{
+    if (!uart_reset(uart))
+    {
+        return false;
+    }
+
+    if (!uart_set_baudrate(uart, baudrate))
+    {
+        return false;
+    }
+
+    if (!uart_set_format(uart, data_bits, stop_bits))
+    {
+        return false;
+    }
+
+    // Set UARTEN, TXE, RXE
+    *uart_info[uart].reg_cr |= 1u | (1u << 8) | (1u << 9);
+
+    return true;
+}
+
+bool uart_disable(uart_t uart)
+{
+    // Clear UARTEN, TXE, RXE
+    *uart_info[uart].reg_cr &= ~(1u | (1u << 8) | (1u << 9));
+}
+
+bool uart_is_enabled(uart_t uart)
+{
+    // Read UARTEN
+    return (*uart_info[uart].reg_cr & 1u) == 1;
+}
+
+bool uart_reset(uart_t uart)
+{
+    switch (uart)
+    {
+        case UART0: reset_subsys(RESET_SUBSYS_UART0); return true;
+        case UART1: reset_subsys(RESET_SUBSYS_UART1); return true;
+        default: return false;
+    }
+}
+
+bool uart_set_baudrate(uart_t uart, uint32_t baudrate)
+{
+    uart_into_t *uart_sel = &uart_info[uart];
+    uint32_t freq = clk_get_freq(CLK_PERI);
+
+    if (freq == 0)
+    {
+        return false;
+    }
+
+    float divisor = (float)freq / (16 * baudrate);
+    uint32_t ibrd = (uint32_t)divisor;
+    uint32_t fbrd = (uint32_t)((divisor - (uint32_t)divisor) * 64 + 0.5);
+
+    *uart_sel->reg_ibrd = ibrd;
+    *uart_sel->reg_fbrd = fbrd;
+
+    return true;
+}
+
+bool uart_set_format(uart_t uart, uint8_t data_bits, uint8_t stop_bits)
+{
+    if (data_bits < 5 || data_bits > 8)
+    {
+        return false;
+    }
+
+    if (stop_bits < 1 || stop_bits > 2)
+    {
+        return false;
+    }
+
+    // Set WLEN (Word Length), FEN (FIFO Enabled), STP2 (Stop Bits)
+    *uart_info[uart].reg_lcr = ((data_bits - 5) << 5) | (1u << 4) | ((stop_bits - 1) << 3);
+
+    return true;
+}
+
+uint8_t uart_read_byte(uart_t uart)
+{
+    uart_into_t *uart_sel = &uart_info[uart];
+
+    // Wait for RXFE (Receive FIFO Empty) to clear
+    while ((*uart_sel->reg_fr & (1u << 4)) != 0);
+
+    return *uart_sel->reg_dr;
+}
+
+uint8_t uart_send_byte(uart_t uart, uint8_t byte)
+{
+    uart_into_t *uart_sel = &uart_info[uart];
+
+    // Wait for TXFF (Transmit FIFO Full) to clear
+    while ((*uart_sel->reg_fr & (1u << 5)) != 0);
+
+    *uart_sel->reg_dr = byte;
+}
---
+++ drv/rp2350/uart.h
@@ -0,0 +1,53 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+#include "common.h"
+#include "clock.h"
+#include "reset.h"
+
+#define UART0_REG_BASE 0x40070000
+#define UART0_REG_DR REG((UART0_REG_BASE + 0x000))
+#define UART0_REG_FR REG((UART0_REG_BASE + 0x018))
+#define UART0_REG_IBRD REG((UART0_REG_BASE + 0x024))
+#define UART0_REG_FBRD REG((UART0_REG_BASE + 0x028))
+#define UART0_REG_LCR REG((UART0_REG_BASE + 0x02c))
+#define UART0_REG_CR REG((UART0_REG_BASE + 0x030))
+
+#define UART1_REG_BASE 0x40078000
+#define UART1_REG_DR REG((UART1_REG_BASE + 0x000))
+#define UART1_REG_FR REG((UART1_REG_BASE + 0x018))
+#define UART1_REG_IBRD REG((UART1_REG_BASE + 0x024))
+#define UART1_REG_FBRD REG((UART1_REG_BASE + 0x028))
+#define UART1_REG_LCR REG((UART1_REG_BASE + 0x02c))
+#define UART1_REG_CR REG((UART1_REG_BASE + 0x030))
+
+typedef enum uart
+{
+    UART0,
+    UART1,
+    UART_COUNT
+} uart_t;
+
+typedef struct uart_info
+{
+    volatile uint32_t *reg_dr;
+    volatile uint32_t *reg_fr;
+    volatile uint32_t *reg_ibrd;
+    volatile uint32_t *reg_fbrd;
+    volatile uint32_t *reg_lcr;
+    volatile uint32_t *reg_cr;
+} uart_into_t;
+
+bool uart_enable(uart_t uart, uint32_t baudrate, uint8_t data_bits, uint8_t stop_bits);
+bool uart_disable(uart_t uart);
+bool uart_is_enabled(uart_t uart);
+bool uart_reset(uart_t uart);
+
+bool uart_set_baudrate(uart_t uart, uint32_t baudrate);
+bool uart_set_format(uart_t uart, uint8_t data_bits, uint8_t stop_bits);
+
+uint8_t uart_read_byte(uart_t uart);
+uint8_t uart_send_byte(uart_t uart, uint8_t byte);
+
+#endif
