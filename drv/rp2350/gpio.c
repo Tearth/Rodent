f9316adc1248e169f60dad7f293cd6ae9e604342
Tearth <tearthdev@gmail.com>
2026-08-06 18:41:56 +0200

    Add GPIO mode selection

--- drv/rp2350/gpio.c
+++ drv/rp2350/gpio.c
@@ -1,13 +1,37 @@
 #include "gpio.h"
 
-gpio_func_t gpio_get_func(uint8_t gpio)
+void gpio_enable(uint8_t gpio)
 {
-    // Read FUNCSEL
-    return *GPIO_REG_CTRL(gpio) & GPIO_FUNC_MASK;
+    // Clear ISO (Pad Isolation Control)
+    *GPIO_PADS_REG_CTRL(gpio) &= ~(1u << 8);
+}
+
+void gpio_disable(uint8_t gpio)
+{
+    // Set ISO (Pad Isolation Control)
+    *GPIO_PADS_REG_CTRL(gpio) |= 1u << 8;
+}
+
+bool gpio_is_enabled(uint8_t gpio)
+{
+    // Read ISO (Pad Isolation Control)
+    return (*GPIO_PADS_REG_CTRL(gpio) & (1u << 8)) == 0;
+}
+
+bool gpio_reset()
+{
+    return reset_subsys(RESET_SUBSYS_PADS_BANK0) && reset_subsys(RESET_SUBSYS_IOBANK0);
 }
 
 void gpio_set_func(uint8_t gpio, gpio_func_t func)
 {
     // Set FUNCSEL
-    *GPIO_REG_CTRL(gpio) = (*GPIO_REG_CTRL(gpio) & ~GPIO_FUNC_MASK) | func;
+    *GPIO_IO_REG_CTRL(gpio) = (*GPIO_IO_REG_CTRL(gpio) & ~GPIO_IO_FUNC_MASK) | func;
+}
+
+void gpio_set_mode(uint8_t gpio, bool input, bool output, bool pull_down, bool pull_up)
+{
+    // Set PDE (Pull Down Enable), PUE (Pull Up Enable), IE (Input Enable), OD (Output Disable)
+    *GPIO_PADS_REG_CTRL(gpio) = (*GPIO_PADS_REG_CTRL(gpio) & ~GPIO_PADS_MODE_MASK) |
+        (pull_down << 2) | (pull_up << 3) | (input << 6) | (!output << 7);
 }
--- drv/rp2350/gpio.h
+++ drv/rp2350/gpio.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 #include "common.h"
+#include "reset.h"
 
-#define GPIO_REG_BASE 0x40028000
-#define GPIO_REG_CTRL(gpio) REG((GPIO_REG_BASE + 0x04 + gpio * 0x08))
+#define GPIO_IO_REG_BASE 0x40028000
+#define GPIO_IO_REG_CTRL(gpio) REG((GPIO_IO_REG_BASE + 0x04 + gpio * 0x08))
+#define GPIO_IO_FUNC_MASK 0x0000001f
 
-#define GPIO_FUNC_MASK 0x0000001f
+#define GPIO_PADS_BANK0_BASE 0x40038000
+#define GPIO_PADS_REG_CTRL(gpio) REG((GPIO_PADS_BANK0_BASE + 0x04 + gpio * 0x04))
+#define GPIO_PADS_MODE_MASK 0x000000ff
 
 typedef enum gpio_func
 {
@@ -28,7 +32,12 @@ typedef enum gpio_func
     GPIO_FUNC_NONE = 31
 } gpio_func_t;
 
-gpio_func_t gpio_get_func(uint8_t gpio);
+void gpio_enable(uint8_t gpio);
+void gpio_disable(uint8_t gpio);
+bool gpio_is_enabled(uint8_t gpio);
+bool gpio_reset();
+
 void gpio_set_func(uint8_t gpio, gpio_func_t func);
+void gpio_set_mode(uint8_t gpio, bool input, bool output, bool pull_down, bool pull_up);
 
 #endif
