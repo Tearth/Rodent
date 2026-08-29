6da1b8d9e6d39c7292cc91bd1eb81416989417c1
Tearth <tearthdev@gmail.com>
2026-08-29 23:03:35 +0200

    Strip kernel ELF

--- tools/scripts/build.sh
+++ tools/scripts/build.sh
@@ -7,5 +7,7 @@ cd ..
 cp ./bin/kernel.elf ./build/fs/sys/kernel.elf
 cp ./etc ./build/fs -r
 
+riscv32-unknown-elf-strip ./build/fs/sys/kernel.elf
+
 # Filesystem
 mklittlefs -c ./build/fs ./bin/fs_img.bin -b 4096 -s $FS_SIZE >/dev/null
